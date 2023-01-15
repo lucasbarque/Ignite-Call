586ef9f9c683d4923e5b2cb01e050bb8603dfe4c
Lucas Barque <lucasbarquedasilva@hotmail.com>
2023-01-14 21:17:43 -0400

    🚧: Next Auth Proccess

---
+++ src/lib/auth/prisma-adapter.ts
@@ -0,0 +1,30 @@
+import { Adapter } from 'next-auth/adapters'
+import { prisma } from '../prisma'
+
+export function PrismaAdapter(): Adapter {
+  return {
+    async createUser(user) {},
+
+    async getUser(id) {
+      const user = await prisma.user.findUnique({
+        where: {
+          id,
+        },
+      })
+
+      return {}
+    },
+    async getUserByEmail(email) {},
+    async getUserByAccount({ providerAccountId, provider }) {},
+    async updateUser(user) {},
+    async deleteUser(userId) {},
+    async linkAccount(account) {},
+    async unlinkAccount({ providerAccountId, provider }) {},
+    async createSession({ sessionToken, userId, expires }) {},
+    async getSessionAndUser(sessionToken) {},
+    async updateSession({ sessionToken }) {},
+    async deleteSession(sessionToken) {},
+    async createVerificationToken({ identifier, expires, token }) {},
+    async useVerificationToken({ identifier, token }) {},
+  }
+}
--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,11 +1,21 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
-import { ArrowRight } from 'phosphor-react'
+import { ArrowRight, Check } from 'phosphor-react'
 import React from 'react'
 import { Container, Header } from '../styles'
-import { ConnectBox, ConnectItem } from './styles'
+import { AuthError, ConnectBox, ConnectItem } from './styles'
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 export default function ConnectCalendar() {
-  // async function handleRegister() {}
+  const session = useSession()
+  const router = useRouter()
+
+  const hasAuthError = !!router.query.error
+  const isSignedIn = session.status === 'authenticated'
+
+  async function handleConnectCalendar() {
+    await signIn('google', { callbackUrl: '/register/connect-calendar' })
+  }
 
   return (
     <Container>
@@ -22,12 +32,30 @@ export default function ConnectCalendar() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size="sm">
-            Conectar
-            <ArrowRight />
-          </Button>
+          {isSignedIn ? (
+            <Button size="sm" disabled>
+              Conectado <Check size="sm" />
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={handleConnectCalendar}
+            >
+              Conectar
+              <ArrowRight />
+            </Button>
+          )}
         </ConnectItem>
-        <Button type="submit">
+
+        {hasAuthError && (
+          <AuthError>
+            Falha ao se conectar ao Google, verifique se você habilitou as
+            permissões de acesso ao Google Calendar.
+          </AuthError>
+        )}
+
+        <Button type="submit" disabled={!isSignedIn}>
           Próximo passo
           <ArrowRight />
         </Button>
