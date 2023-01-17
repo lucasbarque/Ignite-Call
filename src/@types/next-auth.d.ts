dcd719fbe891b5334fa205ed81e8c8acba64acd5
Lucas Barque <lucasbarquedasilva@hotmail.com>
2023-01-16 20:15:43 -0400

    🚧: Next auth progress

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -1,5 +1,11 @@
 import NextAuth from 'next-auth'
 
 declare module 'next-auth' {
-  interface User {}
+  interface User {
+    id: string
+    name: string
+    email: string
+    username: string
+    avatar_url: string
+  }
 }
