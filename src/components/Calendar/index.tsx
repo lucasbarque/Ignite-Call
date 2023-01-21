59a1e23d30dd75d13131715375a9e6f692b505df
Lucas Barque <lucasbarquedasilva@hotmail.com>
2023-01-21 10:55:16 -0400

    🚧: Begin styling calendar

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,58 @@
+import { CaretLeft, CaretRight } from 'phosphor-react'
+import {
+  CalendarActions,
+  CalendarBody,
+  CalendarContainer,
+  CalendarDay,
+  CalendarHeader,
+  CalendarTitle,
+} from './styles'
+import { geetWeekDays } from '@/utils/get-week-days'
+
+export function Calendar() {
+  const shortWeekDays = geetWeekDays({ short: true })
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Janeiro <span>2023</span>
+        </CalendarTitle>
+        <CalendarActions>
+          <button>
+            <CaretLeft />
+          </button>
+          <button>
+            <CaretRight />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDays.map((weekDay) => (
+              <th key={weekDay}>{weekDay}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  )
+}
